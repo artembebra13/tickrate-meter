f46c11bca730cd18e10a4a2d6b256744b611d735
Herosh1 <bebrochka13@bk.ru>
2024-06-01 19:22:05 +0500

    updated void

--- obj/Debug/net48/rust.oxide.live.AssemblyInfo.cs
+++ obj/Debug/net48/rust.oxide.live.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("rust.oxide.live")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47079d6bbb131bf1f5def1651b47261901f72299")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74acc77eab5d7cdd9000eefa6c263f4ad277c7d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("rust.oxide.live")]
 [assembly: System.Reflection.AssemblyTitleAttribute("rust.oxide.live")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- program.cs
+++ program.cs
@@ -2,11 +2,11 @@ using System;
 using Oxide.Plugins;
 
 namespace Oxide.Plugin {
-    [Info("Tickrate meter", "Artem Shumilov a.k.a Herosh1", "0.1")]   
+    [Info("Tickrate meter", "Artem Shumilov a.k.a Herosh1", "0.3")]   
     class program : RustPlugin {
         [ChatCommand("tickrate")]
 
-        void cmdChatRoll (BasePlayer player) {
+        void cmdChatTickrate (BasePlayer player) {
             player.ChatMessage("Текущий тикрейт сервера: " + player.serverTickRate);
         }
     }
